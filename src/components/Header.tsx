c4b3f4205cc82d666d86cde1f45110245f5a8c45
vincenzochiovaro <vincenzochiovaro194@gmail.com>
2023-05-31 11:22:28 +0100

    Implement CategoriesNav component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,23 +4,23 @@ import Navbar from "react-bootstrap/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import "../style/header.css";
+import "../styles/header.css";
 
 type HeaderProps = {};
 
 function Header(props: HeaderProps) {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+    <Navbar bg="white" variant="white" expand="lg" sticky="top">
       <Container>
         <Navbar.Brand>
-          <Link to="/" className="linkStyle">
+          <Link to="/" className="linkStyleHeader">
             Tasty Cards
           </Link>
         </Navbar.Brand>
         <Nav className="ms-auto">
           {/* <Nav.Link style={{ fontWeight: "bold" }}>Bookmarks</Nav.Link> feature to add*/}
 
-          <Link to="/login" className="linkStyle">
+          <Link to="/login" className="linkStyleHeader">
             Sign In <FontAwesomeIcon icon={faSignInAlt} />
           </Link>
         </Nav>
