41f4cfc539cee15601e66b032d3a98764ca32677
vincenzochiovaro <vincenzochiovaro194@gmail.com>
2023-06-01 11:30:57 +0100

    Add new css classes for a better styling

--- src/components/CategoriesNav.tsx
+++ src/components/CategoriesNav.tsx
@@ -1,6 +1,54 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+// import { useEffect, useState } from "react";
+// import { fetchCategories } from "../apiCalls";
+// import { Link } from "react-router-dom";
+// import "../styles/categoriesNav.css";
+
+// type categoryListData = {
+//   id: number;
+//   category_name: string;
+// };
+
+// const CategoriesNav = () => {
+//   const [categoryList, setCategoryList] = useState<categoryListData[] | null>(
+//     null
+//   );
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const categories = await fetchCategories();
+//         setCategoryList(categories);
+//       } catch (error) {
+//         console.log("Error fetching categories:", error);
+//         setCategoryList(null);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <Container>
+//       <Row className="d-flex">
+//         {categoryList?.map((category) => (
+//           <Col data-testid="categoryList" key={category.id}>
+//             <Link
+//               className="linkStyleNav__css"
+//               to={`/recipes/${category.category_name}`}
+//             >
+//               {category.category_name}
+//             </Link>
+//           </Col>
+//         ))}
+//       </Row>
+//     </Container>
+//   );
+// };
+
+// export default CategoriesNav;
+
 import { useEffect, useState } from "react";
 import { fetchCategories } from "../apiCalls";
 import { Link } from "react-router-dom";
@@ -30,20 +78,17 @@ const CategoriesNav = () => {
   }, []);
 
   return (
-    <Container>
-      <Row className="d-flex">
-        {categoryList?.map((category) => (
-          <Col data-testid="categoryList" key={category.id}>
-            <Link
-              className="linkStyleNav__css"
-              to={`/recipes/${category.category_name}`}
-            >
-              {category.category_name}
-            </Link>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <div className="categories-nav">
+      {categoryList?.map((category) => (
+        <Link
+          key={category.id}
+          to={`/recipes/${category.category_name}`}
+          className="category-link animate__animated animate__fadeInUp"
+        >
+          {category.category_name}
+        </Link>
+      ))}
+    </div>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,36 @@
+// import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+// import { Link } from "react-router-dom";
+// import "../styles/header.css";
+
+// type HeaderProps = {};
+
+// function Header(props: HeaderProps) {
+//   return (
+//     <Navbar bg="white" variant="white" expand="lg" sticky="top">
+//       <Container>
+//         <Navbar.Brand>
+//           <Link to="/" className="linkStyleHeader__css">
+//             Tasty Cards
+//           </Link>
+//         </Navbar.Brand>
+//         <Nav className="ms-auto">
+//           {/* <Nav.Link style={{ fontWeight: "bold" }}>Bookmarks</Nav.Link> feature to add*/}
+
+//           <Link to="/login" className="linkStyleHeader__css">
+//             Sign In <FontAwesomeIcon icon={faSignInAlt} />
+//           </Link>
+//         </Nav>
+//       </Container>
+//     </Navbar>
+//   );
+// }
+
+// export default Header;
+
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -5,22 +38,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import "../styles/header.css";
+import "animate.css/animate.min.css";
 
 type HeaderProps = {};
 
 function Header(props: HeaderProps) {
   return (
-    <Navbar bg="white" variant="white" expand="lg" sticky="top">
+    <Navbar
+      bg="white"
+      variant="light"
+      expand="lg"
+      sticky="top"
+      className="header__container"
+    >
       <Container>
-        <Navbar.Brand>
-          <Link to="/" className="linkStyleHeader__css">
+        <Navbar.Brand className="header__animation">
+          <Link to="/" className="header__link ">
             Tasty Cards
           </Link>
         </Navbar.Brand>
         <Nav className="ms-auto">
-          {/* <Nav.Link style={{ fontWeight: "bold" }}>Bookmarks</Nav.Link> feature to add*/}
-
-          <Link to="/login" className="linkStyleHeader__css">
+          <Link to="/login" className="header__link header__signIn">
             Sign In <FontAwesomeIcon icon={faSignInAlt} />
           </Link>
         </Nav>
